e297899473321adb8a1ff783863585eda6ed1a0f
alpaka206 <gyuwon05@gmail.com>
2024-10-09 23:07:57 +0900

    retest

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import HeaderBack from "../components/HeaderBack";
-import Footer from "../components/Footer";
 // 단순한 가이드북 페이지인데 이것도 당시 시간 부족으로 그냥 큰 이미지 하나 넣었습니다. 수정필요해보입니다.
 
 function Guide() {
   return (
     <div className="container">
-      <HeaderBack />
       <div className="content">
         <img
           // className="guide-img"
@@ -19,7 +16,6 @@ function Guide() {
           }}
         />
       </div>
-      <Footer />
     </div>
   );
 }
