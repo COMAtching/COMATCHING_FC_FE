26caca28f8007bbb347b50d38e38ff3a924e8790
ojspp000@naver.com <ojspp000@naver.com>
2024-10-19 02:02:49 +0900

    dw

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -24,7 +24,7 @@ function Mainpage() {
     fetchData();
   }, []);
 
-  return isLoggedIn ? <MainpageLogin /> : <MainpageLogin />;
+  return isLoggedIn ? <MainpageLogin /> : <MainpageUnLogin />;
 }
 
 export default Mainpage;
